bd2455ff920bb505f15344fb8e48f186db4cdcf8
David Homoney <david@homoney.com>
2023-09-01 11:16:48 -0500

    path updates

--- bruteforce._login.py
+++ bruteforce._login.py
@@ -2,8 +2,8 @@ import requests
 import time
 
 host = 'http://<HOST_UP>'
-file1 = open(r'~/API_Sec_Lab/files/FuzzLists/passwordlist.txt', 'r')
-file2 = open(r'~/API_Sec_Lab/files/FuzzLists/loginusernames.txt', 'r')
+file1 = open('./files/FuzzLists/passwordlist.txt', 'r')
+file2 = open('./files/FuzzLists/loginusernames.txt', 'r')
 PasswordList = file1.read().splitlines()
 LoginUserList = file2.read().splitlines()
 
