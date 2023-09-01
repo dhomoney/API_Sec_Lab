f1138b1914f7c6bb429594cdb1c6301a8b3eb948
David Homoney <david@homoney.com>
2023-09-01 11:18:03 -0500

    updated missing file path

--- resource_enum.py
+++ resource_enum.py
@@ -1,7 +1,7 @@
 import requests
 
 host = 'http://10.50.100.45:5000/'
-file1 = open('~/API_Sec_Lab/files/FuzzLists/dirbuster-quick.txt', 'r')
+file1 = open('./files/FuzzLists/dirbuster-quick.txt', 'r')
 Lines = file1.read().splitlines()
 
 count = 0
