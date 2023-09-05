b1e4b2f7e234ef86666a2798b6187d1453ec5a6f
David Homoney <david@homoney.com>
2023-09-05 15:34:28 -0500

    minor syntax change

--- book-id-fuzz.sh
+++ book-id-fuzz.sh
@@ -3,3 +3,4 @@
          sessionid=$(curl -s -X POST -H 'Content-Type: application/json' -d '{"username":"name1","password":"pass1"}' "http://192.168.100.45:5000/users/v1/login" --insecure | cut -d , -f 1 | cut -d \" -f 4)
 
          cat ~/API_Sec_Lab/files/FuzzLists/book-fuzz.txt | while read line; do curl -s -X GET -H 'Connection: close' -H 'Authorization: bearer $sessionid', "message' --insecure "http://<HOST>:<PORT>/books/v1/$(echo $line|tr -d '\n\t\r')" -w '\n'; done
+         
